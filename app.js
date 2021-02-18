e257e3811f31e0d5f70de92f787a3369324f9f82
usman-088 <usmanali14815@gmail.com>
2021-02-18 16:04:30 +0500

    todo

---
+++ app.js
@@ -0,0 +1,84 @@
+
+
+ var inp = document.getElementById('inp')
+
+function add() {
+    // var inp = document.getElementById('inp')
+    var list = document.getElementById('list')
+    // var h3 = document.createElement('h3')
+    var div = document.createElement('div')
+    // var h3text = document.createTextNode(inp.value)
+
+    list.appendChild(div)
+    div.setAttribute('class', 'card')
+    // div.appendChild(h3)
+    // h3.appendChild(h3text)
+
+    var reBtn = document.createElement('button')
+    div.appendChild(reBtn)
+    reBtn.innerHTML = 'Remove'
+    reBtn.setAttribute('class', 'Btnn')
+
+    reBtn.onclick = function () {
+        reBtn.parentNode.remove()
+    }
+
+    var editBtn = document.createElement('button')
+    div.appendChild(editBtn)
+    editBtn.innerHTML = 'Edit'
+    editBtn.setAttribute('class', 'Btnn')
+
+
+    var press = true
+    var prev = ""
+    var input2 = document.createElement('input')
+    var text = document.createTextNode(input2)
+    div.appendChild(input2)
+    div.insertBefore(input2, div.childNodes[0])
+    input2.value = inp.value
+    // h3.remove()
+    input2.disabled = press
+
+    editBtn.onclick = function () {
+
+        if (press) {
+
+            prev = input2.value
+            editBtn.innerHTML = "Update"
+            press = false
+            input2.disabled = press
+            input2.focus
+            input2.onblur = function () {
+                editBtn.innerHTML = "Edit"
+                press = true
+                input2.disabled = press
+            }
+        }
+        else {
+            if (input2.value === "") {
+                input2.value = prev
+            }
+            editBtn.innerHTML = "Edit"
+            press = true
+            input2.disabled = press
+        }
+    }
+
+    inp.value = ""
+}
+
+
+
+function press() {
+   
+
+    if (event.keyCode == 13) {
+        document.getElementById("addBtn").click()
+    }
+
+}
+
+inp.focus()
+
+
+
