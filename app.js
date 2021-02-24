ebad7c70a10619f12918f93191f693373f4f4de0
usman-088 <76055000+usman-088@users.noreply.github.com>
2021-02-24 22:52:48 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,84 +1,37 @@
 
+var db = firebase.database().ref('/user/')
 
- var inp = document.getElementById('inp')
+const getData = () =>{
 
-function add() {
-    // var inp = document.getElementById('inp')
-    var list = document.getElementById('list')
-    // var h3 = document.createElement('h3')
-    var div = document.createElement('div')
-    // var h3text = document.createTextNode(inp.value)
+    let email = document.getElementById('email')
+    let pass = document.getElementById('password')
 
-    list.appendChild(div)
-    div.setAttribute('class', 'card')
-    // div.appendChild(h3)
-    // h3.appendChild(h3text)
+    db.on('value', function (data) {
+            for (var key in data.val()) {
+                var user = data.val()[key];
+                user.key = key;
+        // console.log(user)
+                // console.log(user.email)
+                    if(user.email==email.value && user.password == pass.value ){
+                            window.location = 'todo.html'
 
-    var reBtn = document.createElement('button')
-    div.appendChild(reBtn)
-    reBtn.innerHTML = 'Remove'
-    reBtn.setAttribute('class', 'Btnn')
-
-    reBtn.onclick = function () {
-        reBtn.parentNode.remove()
-    }
-
-    var editBtn = document.createElement('button')
-    div.appendChild(editBtn)
-    editBtn.innerHTML = 'Edit'
-    editBtn.setAttribute('class', 'Btnn')
-
-
-    var press = true
-    var prev = ""
-    var input2 = document.createElement('input')
-    var text = document.createTextNode(input2)
-    div.appendChild(input2)
-    div.insertBefore(input2, div.childNodes[0])
-    input2.value = inp.value
-    // h3.remove()
-    input2.disabled = press
-
-    editBtn.onclick = function () {
-
-        if (press) {
-
-            prev = input2.value
-            editBtn.innerHTML = "Update"
-            press = false
-            input2.disabled = press
-            input2.focus
-            input2.onblur = function () {
-                editBtn.innerHTML = "Edit"
-                press = true
-                input2.disabled = press
+                }else if(email.value == ''){
+                    alert('Plz enter email')
+                }
+                else if(pass.value == ''){
+                    alert('Plz enter password')
+                }else if (user.email!=email.value || user.password != pass.value)
+                alert('Plz enter coreect email & password')
             }
-        }
-        else {
-            if (input2.value === "") {
-                input2.value = prev
-            }
-            editBtn.innerHTML = "Edit"
-            press = true
-            input2.disabled = press
-        }
-    }
 
-    inp.value = ""
-}
-
-
-
-function press() {
-   
-
-    if (event.keyCode == 13) {
-        document.getElementById("addBtn").click()
-    }
+        })
 
 }
 
-inp.focus()
-
+const clicked = () =>{
+    if(event.keyCode == 13){
+        document.getElementById('loginBtn').click()
+    }
+}
 
 
