2db827ca2072b21d11c4e4a7abeac66e0a9dfae2
usman-088 <76055000+usman-088@users.noreply.github.com>
2021-03-02 18:08:19 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,22 +6,31 @@ const getData = () =>{
     let email = document.getElementById('email')
     let pass = document.getElementById('password')
 
+    var found=false
     db.on('value', function (data) {
+            
             for (var key in data.val()) {
                 var user = data.val()[key];
                 user.key = key;
-        // console.log(user)
-                // console.log(user.email)
                     if(user.email==email.value && user.password == pass.value ){
+                        
+                            found=true
                             window.location = 'todo.html'
+                            
+                            break;
 
                 }else if(email.value == ''){
                     alert('Plz enter email')
+                    break
                 }
                 else if(pass.value == ''){
                     alert('Plz enter password')
-                }else if (user.email!=email.value || user.password != pass.value)
-                alert('Plz enter coreect email & password')
+                    break
+                }
+            }
+            if(!found){
+
+                alert('Plz enter email and password')
             }
 
         })
